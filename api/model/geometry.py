fdb7d5ca76917eb65109e75cc6cd309dfe8ec10e
jyucsiro <4723726+jyucsiro@users.noreply.github.com>
2019-12-16 17:20:58 +1100

    adding extra views

--- api/model/geometry.py
+++ api/model/geometry.py
@@ -5,13 +5,28 @@ from rdflib.namespace import NamespaceManager
 import _config as config
 import json
 from shapely.geometry import shape
+from pprint import pprint
+import psycopg2
+import os
+
+
 
 GeometryView = View("GeometryView", "A profile of geometry.", ['text/html', 'application/json', 'text/turtle', 'text/plain'],
                  'text/html', namespace="http://example.org/def/geometryview")
 
+CentroidView = View("CentroidView", "A profile of geometry's centroid.", ['text/html', 'application/json', 'text/turtle', 'text/plain'],
+                 'text/html', namespace="http://example.org/def/centroidview")
+
+SimplifiedGeomView = View("SimplifiedGeomView", "A profile of the geometry that has been simplified.", ['text/html', 'application/json', 'text/turtle', 'text/plain'],
+                 'text/html', namespace="http://example.org/def/simplifiedgeomview")
+
 class GeometryRenderer(Renderer):
     def __init__(self, request, uri, instance, geom_html_template, **kwargs):
-        self.views = {'geometryview': GeometryView}
+        self.views = {
+                       'geometryview': GeometryView,
+                       'centroid': CentroidView,
+                       'simplifiedgeom': SimplifiedGeomView 
+                     }
         self.default_view_token = 'geometryview'
         super(GeometryRenderer, self).__init__(
             request, uri, self.views, self.default_view_token, **kwargs)
@@ -29,10 +44,54 @@ class GeometryRenderer(Renderer):
             return Response(self._geojson_to_wkt(),
                             mimetype="text/plain", status=200)
         elif self.format == "text/html":
-            return Response(render_template(self.geom_html_template, **self.instance, uri=self.uri, request=self.request))
+            return Response(render_template(self.geom_html_template, **self.instance, uri=self.uri, request=self.request, view=self.view))
         elif self.format == "text/turtle":
             return Response(self.export_rdf(self, rdf_mime='text/turtle'),
                             mimetype="application/json", status=200)
+
+    def _render_centroidview(self):
+        self.headers['Profile'] = 'http://example.org/def/centroidview'
+        self._fetch_geom_centroid_from_db()
+        if self.format == "application/json":
+            return Response(json.dumps(self.instance),
+                            mimetype="application/json", status=200)
+        if self.format == "text/plain":
+            return Response(self._geojson_to_wkt(),
+                            mimetype="text/plain", status=200)
+        elif self.format == "text/html":
+            return Response(render_template(self.geom_html_template, **self.instance, uri=self.uri, request=self.request, view=self.view))
+        elif self.format == "text/turtle":
+            return Response(self.export_rdf(self, rdf_mime='text/turtle'),
+                            mimetype="application/json", status=200)
+
+    def _render_simplifiedgeomview(self):
+        self.headers['Profile'] = 'http://example.org/def/simplifiedgeomview'
+        self._fetch_simplified_geom_from_db()
+        if self.format == "application/json":
+            return Response(json.dumps(self.instance),
+                            mimetype="application/json", status=200)
+        if self.format == "text/plain":
+            return Response(self._geojson_to_wkt(),
+                            mimetype="text/plain", status=200)
+        elif self.format == "text/html":
+            return Response(render_template(self.geom_html_template, **self.instance, uri=self.uri, request=self.request, view=self.view))
+        elif self.format == "text/turtle":
+            return Response(self.export_rdf(self, rdf_mime='text/turtle'),
+                            mimetype="application/json", status=200)
+
+    def _render_alternates_view_html(self):
+        return Response(
+            render_template(
+                self.alternates_template or 'alternates.html',
+                register_name='Geometry Register',
+                class_uri='http://www.opengis.net/ont/geosparql#Geometry',
+                instance_uri='',
+                default_view_token=self.default_view_token,
+                views=self.views
+            ),
+            headers=self.headers
+        )
+
 
     def export_rdf(self, model_view='geometryview', rdf_mime='text/turtle'):
         g = Graph()
@@ -62,11 +121,81 @@ class GeometryRenderer(Renderer):
     def _get_rdf_mimetype(self, rdf_mime):
         return self.RDF_SERIALIZER_MAP[rdf_mime]
 
+    def _fetch_geom_centroid_from_db(self):
+      """
+         Assumes there is a Postgis database with connection config specified in system environment variables.
+         Also assumes there is a table/view called 'combined_geoms' with structure (id, dataset, geom).
+         This function connects to the DB, and queries for the geom as geojson based on input dataset and geom_id parameters.
+      """
+      geom_id = self.instance['id']
+      dataset = self.instance['dataset']
+      db_name = os.environ['GSDB_DBNAME']
+      db_host = os.environ['GSDB_HOSTNAME']
+      db_port = os.environ['GSDB_PORT']
+      username = os.environ['GSDB_USER']
+      passwd = os.environ['GSDB_PASS']
+      conn = psycopg2.connect(dbname=db_name, host=db_host, port=db_port, user=username, password=passwd)
+      cur = conn.cursor()
+      query = 'select id, dataset, ST_AsGeoJSON( ST_Centroid(ST_Transform(geom,4326)) ) from combined_geoms where id = \'{id}\' and dataset=\'{dataset}\';'.format(id=geom_id, dataset=dataset)
+      backup_query = 'select id, dataset, ST_AsGeoJSON( ST_Centroid(geom) ) from combined_geoms where id = \'{id}\' and dataset=\'{dataset}\';'.format(id=geom_id, dataset=dataset)
+      try:
+         cur.execute(query)
+      except Exception as e:
+           print(e)
+           conn.rollback()
+           cur.execute(backup_query)
+      (id,dataset,geojson) = cur.fetchone()
+      o = json.loads(geojson)
+      o['id'] = geom_id
+      o['dataset'] = dataset
+      self.instance = o
+      return o
+
+    def _fetch_simplified_geom_from_db(self):
+      """
+         Assumes there is a Postgis database with connection config specified in system environment variables.
+         Also assumes there is a table/view called 'combined_geoms' with structure (id, dataset, geom).
+         This function connects to the DB, and queries for the geom as geojson based on input dataset and geom_id parameters.
+      """
+      geom_id = self.instance['id']
+      dataset = self.instance['dataset']
+      db_name = os.environ['GSDB_DBNAME']
+      db_host = os.environ['GSDB_HOSTNAME']
+      db_port = os.environ['GSDB_PORT']
+      username = os.environ['GSDB_USER']
+      passwd = os.environ['GSDB_PASS']
+      conn = psycopg2.connect(dbname=db_name, host=db_host, port=db_port, user=username, password=passwd)
+      cur = conn.cursor()
+      query = 'select id, dataset, ST_AsGeoJSON( ST_Transform( ST_Simplify(geom, 100, true),4326) ) from combined_geoms where id = \'{id}\' and dataset=\'{dataset}\';'.format(id=geom_id, dataset=dataset)
+      backup_query = 'select id, dataset, ST_AsGeoJSON( ST_Simplify(geom, 100) ) from combined_geoms where id = \'{id}\' and dataset=\'{dataset}\';'.format(id=geom_id, dataset=dataset)
+      try:
+         cur.execute(query)
+      except Exception as e:
+           print(e)
+           conn.rollback()
+           cur.execute(backup_query)
+      print(query)
+      (id,dataset,geojson) = cur.fetchone()
+      o = json.loads(geojson)
+      o['id'] = geom_id
+      o['dataset'] = dataset
+      self.instance = o
+      return o
+
+
     # All `Renderer` subclasses _must_ implement render
     def render(self):
+        if hasattr(self, 'vf_error'):
+            return Response(self.vf_error, status=406, mimetype='text/plain')
         response = super(GeometryRenderer, self).render()
         if not response and self.view == 'geometryview':
             response = self._render_geometryview()
+        elif not response and self.view == 'simplifiedgeom':
+            response = self._render_simplifiedgeomview()
+        elif not response and self.view == 'centroid':
+            response = self._render_centroidview()
+        elif self.view == 'alternates':
+            response = self._render_alternates_view_html()
         else:
             raise NotImplementedError(self.view)
         return response
