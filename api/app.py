c8110c1b8cc6f113700c2f14f6a5fc05a998103a
Jonathan Yu <jonathan.yu@csiro.au>
2020-02-20 22:30:54 +1100

    adding openapi using boilerplate

--- api/app.py
+++ api/app.py
@@ -3,12 +3,27 @@ import _config as conf
 import pyldapi
 from flask import Flask
 from controller import pages, classes
-
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__, template_folder=conf.TEMPLATES_DIR, static_folder=conf.STATIC_DIR)
+CORS(app)
 
 app.register_blueprint(pages.pages)
-app.register_blueprint(classes.classes)
+app.register_blueprint(classes.classes, url_prefix='/api/v1')
+
+### swagger specific ###
+SWAGGER_URL = '/api/doc'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Geometry Data Service API"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 
 
 # run the Flask app
